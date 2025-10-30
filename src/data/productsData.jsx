de59fc3bbe1d818970d6a36a7b83ccc00fd10fc6
selion-wizard <hexxreg@gmail.com>
2025-10-30 18:10:15 +0500

    Второе задание

---
+++ src/data/productsData.jsx
@@ -0,0 +1,44 @@
+export const productsData = [
+  {
+    id: 1,
+    image: '/images/image_k1.jpeg', 
+    title: 'Кроссовки',
+    description: 'Удобные спортивные кроссовки для бега',
+    price: '5 990 ₽'
+  },
+  {
+    id: 2,
+    image: '/images/image_k1.jpeg', 
+    title: 'Кроссовки',
+    description: 'Удобные спортивные кроссовки для бега',
+    price: '12 500 ₽'
+  },
+  {
+    id: 3,
+    image: '/images/image_k1.jpeg',
+    title: 'Кроссовки',
+    description: 'Удобные спортивные кроссовки для бега',
+    price: '8 990 ₽'
+  },
+  {
+    id: 4,
+    image: '/images/image_k1.jpeg',
+    title: 'Кроссовки',
+    description: 'Удобные спортивные кроссовки для бега',
+    price: '3 490 ₽'
+  },
+  {
+    id: 5,
+    image: '/images/image_k1.jpeg',
+    title: 'Кроссовки',
+    description: 'Удобные спортивные кроссовки для бега',
+    price: '2 990 ₽'
+  },
+  {
+    id: 6,
+    image: '/images/image_k1.jpeg',
+    title: 'Кроссовки',
+    description: 'Удобные спортивные кроссовки для бега',
+    price: '15 990 ₽'
+  }
+];
